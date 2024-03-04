e8d4648dae57b8b7c4aa49a7d11c7e042ff51fea
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-03-04 20:00:38 +0300

    Update models.js

--- server/models/models.js
+++ server/models/models.js
@@ -3,24 +3,24 @@ const sequelize = require("../db");
 
 const ProfessorModel = sequelize.define("professor", {
 	id: {type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true},
-	last_name: {type: DataTypes.STRING(50), allowNull: false},
-	first_name: {type: DataTypes.STRING(50), allowNull: false},
-	patronymic: {type: DataTypes.STRING(50), allowNull: false},
-	email: {type: DataTypes.STRING(50), allowNull: false, unique: true},
-	phone: {type: DataTypes.STRING(50), allowNull: false, unique: true},
+	last_name: {type: DataTypes.STRING, allowNull: false},
+	first_name: {type: DataTypes.STRING, allowNull: false},
+	patronymic: {type: DataTypes.STRING, allowNull: false},
+	email: {type: DataTypes.STRING, allowNull: false, unique: true},
+	phone: {type: DataTypes.STRING, allowNull: false, unique: true},
 	num_of_thesis: {type: DataTypes.INTEGER, defaultValue: 0},
 });
 
 const StudentModel = sequelize.define("student", {
 	id: {type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true},
-	last_name: {type: DataTypes.STRING(50), allowNull: false},
-	first_name: {type: DataTypes.STRING(50), allowNull: false},
-	patronymic: {type: DataTypes.STRING(50), allowNull: false},
+	last_name: {type: DataTypes.STRING, allowNull: false},
+	first_name: {type: DataTypes.STRING, allowNull: false},
+	patronymic: {type: DataTypes.STRING, allowNull: false},
 });
 
 const ThesisModel = sequelize.define("thesis", {
-	id: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
-	title: {type: DataTypes.STRING(50), allowNull: false},
+	id: {type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true},
+	title: {type: DataTypes.STRING, allowNull: false},
 });
 
 ProfessorModel.hasMany(ThesisModel, {
