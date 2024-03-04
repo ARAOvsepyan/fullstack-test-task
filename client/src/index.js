e8212d9314626bc5bc8d503b4dc98fa0e0642b3f
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-03-04 20:00:59 +0300

    Create index.js

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React, { createContext } from 'react';
+import ReactDOM  from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+
+import App from './App';
+
+export const Context = createContext(null);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+)
