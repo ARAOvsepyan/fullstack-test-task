cf55063d45d92d9e76ab3488846efda4a9eb3f57
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-03-04 20:00:24 +0300

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Topbar from "./scenes/global/TopBar";
+import SideBar from "./scenes/global/SideBar";
+import AppRouter from './components/AppRouter';
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+
+const App = () => {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className='app'>
+            <SideBar />
+          <main className='content'>
+            <Topbar />
+            <AppRouter />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
+}
+
+export default App
