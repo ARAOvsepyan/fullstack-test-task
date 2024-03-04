121cac779dea2d32eee42cf3960d510a2a3962f9
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-03-04 20:01:06 +0300

    Create ProfessorsGrid.jsx

---
+++ client/src/components/ProfessorsGrid.jsx
@@ -0,0 +1,85 @@
+import React, {useState, useMemo, useCallback} from 'react';
+import { Box, Typography } from '@mui/material'
+
+import { AgGridReact } from 'ag-grid-react';
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-quartz.css";
+import 'ag-grid-enterprise'
+
+const ProfessorsGrid = () => {
+  const [gridApi, setGridApi] = useState(null)
+  
+  const [professorColData] = useState([
+    { field: "last_name", headerName: 'Имя', filter: 'agTextColumnFilter' },
+    { field: "first_name", headerName: 'Фамилия', filter: 'agTextColumnFilter'},
+    { field: "patronymic", headerName: 'Отчество', filter: 'agTextColumnFilter'},
+    { field: "email", headerName: 'Почта', filter: 'agTextColumnFilter', minWidth: 230},
+    { field: "phone", headerName: 'Телефон', filter: 'agNumberColumnFilter'}
+  ]);
+
+  const datasource = {
+    getRows(params) {
+      const { startRow, endRow, filterModel, sortModel } = params
+      let url = `http://localhost:8000/api/professors?`
+      if (sortModel.length) {
+        const { colId, sort } = sortModel[0]
+        url += `sort=${colId}&validate=${sort}&`
+      }
+      const filterKeys = Object.keys(filterModel)
+      filterKeys.forEach(filter => {
+        url += `${filter}=${filterModel[filter].filter}&`
+      })
+      url += `page=${startRow}&per_page=${endRow}`
+      fetch(url)
+        .then(httpResponse => httpResponse.json())
+        .then(response => {
+          params.successCallback(response.rows, response.count);
+        })
+        .catch(error => {
+          console.error(error);
+          params.failCallback();
+        })
+    }
+  };
+
+  const onGridReady = (params) => {
+    setGridApi(params);
+    params.api.setDatasource(datasource);
+  }
+
+  const components={
+    loading:(params)=>{
+      if(params.value!==undefined){
+        return params.value
+      }else{
+        return "<img src='https://www.ag-grid.com/example-assets/loading.gif'/>"
+      }
+    }
+  }
+
+  const autoSizeStrategy = useMemo(() => {
+    return {
+      type: 'fitCellContents',
+    };
+  }, []);
+
+  return (
+    <Box
+      className='ag-theme-quartz'
+      height='700px'
+      width='100%'
+    > 
+      <Typography variant='h2' marginBottom={2.3}>Профессора</Typography>
+      <AgGridReact
+        columnDefs={professorColData}
+        onGridReady={onGridReady}
+        rowModelType={'infinite'}
+        components={components}
+        defaultColDef={{ filter: true, floatingFilter: true, sortable: true }}
+        autoSizeStrategy={autoSizeStrategy}
+      />
+    </Box>
+  )
+};
+
+export default ProfessorsGrid;
