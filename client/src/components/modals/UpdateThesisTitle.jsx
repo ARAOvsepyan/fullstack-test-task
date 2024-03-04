c5cc8ec60a6f669c2577df9aeead23ba25497b94
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-03-04 20:00:32 +0300

    Create UpdateThesisTitle.jsx

---
+++ client/src/components/modals/UpdateThesisTitle.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { Dialog, DialogContentText, useTheme, DialogActions, TextField, Button, DialogContent, Typography } from '@mui/material';
+import { tokens } from '../../theme';
+
+const UpdateThesisTitle = ({open, handleClose, data, onChange, updateRow, current}) => {
+  const {title} = data;
+
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode)
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+    >
+      <DialogContent 
+        sx={{
+          backgroundColor: colors.blueAccent[500],
+        }}
+      >
+        <DialogContentText>
+          <Typography variant='p' color='white' fontWeight='bold'>Изменить тему дипломной работы</Typography>
+        </DialogContentText>
+        <TextField
+          autoFocus
+          margin='normal'
+          id='title'
+          value={title}
+          onChange={e => onChange(e)}
+          label={current}
+          type='text'
+          fullWidth
+        />
+        <DialogActions>
+          <Button type='submit' onClick={() => updateRow()}>
+            <Typography variant='h5' color='white' fontWeight='bold'>
+              ИЗМЕНИТЬ
+            </Typography>
+          </Button>
+        </DialogActions>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default UpdateThesisTitle;
