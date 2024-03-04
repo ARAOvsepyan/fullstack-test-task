4bd1c4e344e3a8fb1e275ae818f3412be93ca562
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-03-04 20:00:40 +0300

    Update thesisDto.js

--- server/dtos/thesisDto.js
+++ server/dtos/thesisDto.js
@@ -1,11 +1,12 @@
-module.exports = class ThesisDto{
+const PersonEntity = require("./personEntity");
+
+module.exports = class ThesisDto extends PersonEntity{
 	title;
 	professorId;
-	studentId;
 
 	constructor(model) {
+		super({...model});
 		this.title = model.title;
 		this.professorId = model.professorId;
-		this.studentId = model.studentId;
 	}
 };
